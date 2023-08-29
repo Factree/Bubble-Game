65aa131c48b5d2faf51e852a033fa750f804239e
Deepak Dhawan <deepakdhawan@outlook.com>
2023-08-29 23:28:19 +0530

    Bubbles with random number generated

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-for(var i = 1; i<=70; i++){
-    <div class="bubble">5</div>
+var bubbles = "";
+var minB = 1 ;
+var maxB = 400;
+for(var i = 1; i<=maxB; i++){
+    var rn = Math.floor(Math.random()* (maxB - minB +1)) + minB;
+    bubbles += `<div class="bubble">${rn}</div>`;
 }
+document.querySelector(".pbtm").innerHTML = bubbles;
